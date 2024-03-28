0981931d164e614b2687e9414c08383f63b19864
jhchoi1182 <jhchoi1182@gmail.com>
2024-03-29 00:35:51 +0900

    feat: 그냥 평균값 계산하는 input 추가

--- src/components/PerformanceCalculator/OutlierExcludedAverageInput.jsx
+++ src/components/PerformanceCalculator/OutlierExcludedAverageInput.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { useState } from "react";
 
-export default function CalculateAverageInput() {
+export default function OutlierExcludedAverageInput() {
   const [input, setInput] = useState("");
   const [average, setAverage] = useState(0);
 
@@ -11,20 +12,25 @@ export default function CalculateAverageInput() {
       .split(", ")
       .map(Number)
       .filter((n) => !isNaN(n));
-    if (numbers.length <= 0) {
-      alert("최소 1개 이상의 숫자를 입력해주세요.");
+    if (numbers.length <= 2) {
+      alert("최소 3개 이상의 숫자를 입력해주세요.");
       return;
     }
 
+    // 최대값과 최소값을 제거
+    const maxVal = Math.max(...numbers);
+    const minVal = Math.min(...numbers);
+    const filteredNumbers = numbers.filter((n) => n !== maxVal && n !== minVal);
+
     // 평균 계산
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-    const avg = sum / numbers.length;
+    const sum = filteredNumbers.reduce((acc, curr) => acc + curr, 0);
+    const avg = sum / filteredNumbers.length;
     setAverage(avg);
   };
 
   return (
     <div>
-      <h2>평균값 구하기</h2>
+      <h2>이상치를 제외한 평균값 구하기</h2>
       <input style={{ width: "400px" }} type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="숫자들을 입력하세요 (예: 23.34, 21.5, ...)" />
       <button onClick={calculateAverage}>평균 계산</button>
       {average !== null && <p>계산된 평균: {average.toFixed(2)}</p>}
--- src/components/PerformanceCalculator/PerformanceCalculator.jsx
+++ src/components/PerformanceCalculator/PerformanceCalculator.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CalculateAverageInput from "./CalculateAverageInput";
+import OutlierExcludedAverageInput from "./OutlierExcludedAverageInput";
 import CalculateRerenderDurationInput from "./CalculateRerenderDurationInput";
 
 export default function PerformanceCalculator() {
   return (
     <div>
       <CalculateAverageInput />
+      <OutlierExcludedAverageInput />
       <CalculateRerenderDurationInput />
     </div>
   );
